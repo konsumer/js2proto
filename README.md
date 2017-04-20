fc3220f2dd927946b731ce1678d3501898b02feb
David Konsumer <konsumer@jetboystudio.com>
2017-04-19 20:23:25 -0700

    initial code

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # js2proto
+
 Dumb utility to convert a JSON object into a protobuf message definition
+
+You could be using a struct to describe the general concept of JSON, but this will try to guess the defined scalar types, so you can quickly build a spec for some random JSON.
+
+## install
+
+Install this with `npm i -g js2proto`.
+
+## usage
+
+js2proto takes JSON on stdin, and outputs a protobuf to stdout.
+
+
+```
+cat MYFILE.json | js2proto TypeName > typename.proto
+```
+
+## notes
+
+
+
+## example
+
+```json
+{
+  "testNum": 5,
+  "testString": "tester",
+  "testBool": true,
+  "testArray": [1,2,3,4],
+  "testObj": {
+    "testNum": 5,
+    "testString": "tester",
+    "testBool": true,
+    "testArray": [1,2,3,4]
+  }
+}
+```
+
+becomes
+
+```proto
+syntax = "proto3";
+
+message Test {
+  uint32 testNum = 1;
+  string testString = 2;
+  bool testBool = 3;
+  repeated uint32 testArray = 4;
+  message InnerType1 {
+    uint32 testNum = 1;
+    string testString = 2;
+    bool testBool = 3;
+    repeated uint32 testArray = 4;
+  }
+  InnerType1 testObj = 5;
+}
+```
---
+++ js2proto.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+if (!process.argv[2]) {
+  console.error(`
+Usage: cat MYFILE.json | js2proto TypeName > typename.proto
+
+Where
+  MYFILE.json is your JSON file
+  TypeName is the name of your message-type
+  typename.proto is the name of the file you want to output
+
+`)
+  process.exit(1)
+}
+
+let innerCount = 0
+
+const isFloat = n => n === +n && n !== (n | 0)
+const getProtoType = (obj) => {
+  const t = typeof obj
+  if (t === 'number') {
+    if (isFloat(obj)) {
+      return 'float'
+    } else {
+      return obj < 0 ? 'sint32' : 'uint32'
+    }
+  }
+
+  if (t === 'object') {
+    if (Array.isArray(obj)) {
+      return 'repeated ' + getProtoType(obj[0])
+    } else {
+      innerCount++
+      return getTypesForObject(obj, `InnerType${innerCount}`).split('\n').join('\n  ') + `\n  InnerType${innerCount}`
+    }
+  }
+
+  if (t === 'string') {
+    if (Number(obj) == obj) {
+      return 'int64'
+    }
+    return 'string'
+  }
+
+  if (t === 'boolean') {
+    return 'bool'
+  }
+}
+
+const getTypesForObject = (js, name) => {
+  return `message ${name} {\n` + Object.keys(js).map((k, i) => `  ${getProtoType(js[k])} ${k} = ${i + 1};`)
+  .join('\n') + '\n}'
+}
+
+let out = ''
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read()
+  if (chunk !== null) {
+    out += chunk.toString()
+  }
+})
+process.stdin.on('end', () => { console.log('syntax = "proto3";\n\n' + getTypesForObject(JSON.parse(out), process.argv[2])) })
